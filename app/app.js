94817fcaeca7b8fb54835ae04c425a37b0ed798c
Jan Krzeptowski <saj96n@gmail.com>
2017-01-06 14:12:54 +0100

    Add query caching

--- app/app.js
+++ app/app.js
@@ -112,34 +112,46 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $auth
     });
 
 // rest service
-app.factory('StationsService', function($resource) {
+app.factory('StationsService', function($resource, $cacheFactory) {
     var Api = "http://localhost:3000/api/";
     var messages = {};
 
     return {
         query: function(surl, afterf) {
-            return $resource(Api + surl).query(function() {
-                afterf
+            return $resource(Api + surl, {}, {
+                query: {
+                    isArray: true,
+                    cache: true,
+                    method: 'GET'
+                }
+            }).query(function() {
+                afterf;
             });
         },
         messages: messages,
         post: function(data) {
             $resource(Api + "logs").save(data,
                 function(resp, headers) {
-                    //success callback
+                    // success callback
                     messages.success = "Item has been successfully added";
+                    // clean cache after adding new item
+                    $cacheFactory.get('$http').removeAll();
                 },
                 function(err) {
                     messages.error = "Error occured";
                 });
         },
         put: function(data) {
-            $resource(Api + "logs",{},{update: {
-              method: 'PUT'
-            }}).update(data,
+            $resource(Api + "logs", {}, {
+                update: {
+                    method: 'PUT'
+                }
+            }).update(data,
                 function(resp, headers) {
-                    //success callback
+                    // success callback
                     messages.success = "Item has been successfully modified";
+                    // clean cache after modifying item
+                    $cacheFactory.get('$http').removeAll();
                 },
                 function(err) {
                     messages.error = "Error occured";
